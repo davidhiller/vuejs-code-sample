09531c8785ccb3abbae27a7558a228366943e3c8
David Hiller <75601214+davidhiller@users.noreply.github.com>
2021-02-11 00:29:32 -0600

    Add Ipsum text to mock jobs data
    Make jobs data more descriptive to aid development

--- src/JobsApiHelper.js
+++ src/JobsApiHelper.js
@@ -5,27 +5,27 @@ import Vue from "vue";
 export default Vue.observable({
   jobs: [
     {
-      id: 1,
-      title: "Job A",
-      description: "Description for Job A",
+      id: 11,
+      title: "Lead Worker - Sous Chef",
+      description: "Key players will take ownership of their propositions by virtually facilitating actionable stakeholders. So we can hit the ground running, we will be iteratively strategizing every milestone in our space. Our business incentivizes standpoints to strategically and intelligently align our next-generation step-change. Our organic growth development lifecycle enables best-of-breed, seamless verticals.",
       skills: ["Skill Address A1", "Skill A2", "Skill A3", "Skill A4"]
     },
     {
-      id: 2,
+      id: 21,
       title: "Job B",
-      description: "Description for Job B",
+      description: "You need to proactively monetize your platforms to increase your prince2 practitioner velocity. Our synergy development lifecycle enables unparalleled, senior visibilities. We thrive because of our corporate enterprise and knowledge transfer stand-up culture. Our business relays innovations to reliably and conservatively calibrate our long-term industry leader.",
       skills: ["Skill Xylophone B1", "Skill B2"]
     },
     {
-      id: 3,
+      id: 30,
       title: "Job C",
-      description: "Description for Job C",
+      description: "In the future, will you be able to globally deep-dive team players in your business? In the future, will you be able to effectively synergise market foci in your business? It's critical that we give 110% when proactively integrating enterprises. Key players will take ownership of their paradigm shifts by intelligently reusing innovative executive searches.",
       skills: ["Skill Catapult C1", "Skill C2", "Skill C3"]
     },
     {
-      id: 4,
+      id: 47,
       title: "Job D",
-      description: "Description for Job D",
+      description: "Proactively transforming iteratively knowledge transfer user experiences is crucial to our long-term core asset. It's critical that we give 110% when ethically growing core competencies. Value-added industry leaders are becoming world-class silo experts. Key players will take ownership of their alignments by dynamically leveraging proactive stand-ups.",
       skills: [
         "Skill D1",
         "Skill D2",
@@ -35,15 +35,15 @@ export default Vue.observable({
       ]
     },
     {
-      id: 5,
+      id: 53,
       title: "Job E",
-      description: "Description for Job E",
+      description: "We aim to conservatively revolutionize our step-change by reliably virtualising our best-of-breed innovative emerging markets. Change the way you do business - adopt immersive brands. Company-wide propositions are becoming actionable milestone experts. In the future, will you be able to intelligently impact stacks in your business?",
       skills: ["Skill Dress E1"]
     },
     {
-      id: 6,
+      id: 60,
       title: "Job F",
-      description: "Description for Job F",
+      description: "Our business relays clouds to dynamically and conservatively incentivize our wholesale stakeholder. Key players will take ownership of their market foci by intelligently strategizing end-to-end synergies. We use our senior paradigm shifts to effectively manage our organic growth expectations. Is your standpoint prepared for unparalleled enterprise growth?",
       skills: ["Skill F1", "Skill F2"]
     }
   ]
